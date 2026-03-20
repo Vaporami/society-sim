07eb71ff7149e6ea6c264274c6eb9b63949357e0
vapori <vaporamireive@gmail.com>
2026-03-20 07:30:01 +0300

    To show to my friend

--- src/Person.cs
+++ src/Person.cs
@@ -4,20 +4,34 @@ namespace society_sim;
 internal class Person
 {
     static private int _lastId = -1;
-    static List<string> _emotions;
+
+    static private List<string> _emotions;
     static private Dictionary<string, Dictionary<string, double>> _emotionsCoeffsDict;
 
     private int _id;
     private string _name;
     private int _age;
+    
+    public Person(string name = "UNNAMED", int age = 18)
+    {
+        this._id = ++Person._lastId;
+        this._name = name;
+        this._age = age;
+    }
 
     static private void InitStaticPerson(string pathToEmotionsJSON)
     {
+        Person._emotions = new List<string>();
+        Person._emotionsCoeffsDict =
+            new Dictionary<string, Dictionary<string, double>>(Person._emotions.Count);
+        
         string text = string.Empty;
         using (StreamReader reader = new(pathToEmotionsJSON))
+        {
             text = reader.ReadToEnd();
+        }
         JsonDocument json = JsonDocument.Parse(text);
-        JsonElement root = JsonDocument.RootElement;
+        JsonElement root = json.RootElement;
 
         JsonElement emotions = root.GetProperty("Emotions");
         if (emotions.ValueKind != JsonValueKind.Array)
@@ -26,24 +40,20 @@ internal class Person
             return;
         }
         
-        foreach (string val in emotions.Item)
+        foreach (JsonElement el in emotions.EnumerateArray())
         {
-            _emotions.Add(val);
+            _emotions.Add(el.GetString());
         }
 
         JsonElement coeffs = root.GetProperty("CoeffsDict");
-        foreach (string emotion in Person._emotions)
+        foreach (JsonElement el in coeffs.EnumerateObject())
         {
             Dictionary<string, double> innerDict = new();
-            Person._emotionsCoeffsDict.Add(emotion, innerDict)
+            foreach (JsonElement innerEl in el.EnumerateObject())
+            {
+                // innerDict.Add(innerEl
+            }
         }
-    }
-        
-    public Person(string name = "UNNAMED", int age = 18)
-    {
-        this._id = ++Person._lastId;
-        this._name = name;
-        this._age = age;
     }
 
     public override string ToString()
@@ -51,4 +61,10 @@ internal class Person
         return $"[{this._id} {this._name} {this._age}]";
     }
 }
+
+
+
+
+
+
 
