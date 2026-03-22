29eb60dfccd578970d62a1f744b0527ea94c8a7f
vapori <vaporamireive@gmail.com>
2026-03-22 13:29:27 +0300

    Changed the location of the submodule.

---
+++ external/Utilities/src/JsonUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace Utilities;
+
+public sealed class JsonUtils
+{
+    static public JsonDocument GetDocFromFile(string path)
+    {
+        string text = string.Empty;
+        using (StreamReader reader = new(path))
+        {
+            text = reader.ReadToEnd();
+        }
+        return JsonDocument.Parse(text);
+    }
+
+    static public JsonElement GetRootFromFile(string path)
+    {
+        return JsonUtils.GetDocFromFile(path).RootElement;
+    }
+} // testing
